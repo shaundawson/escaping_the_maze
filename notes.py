d8da11b78b241a7c43b41a3229d030d78e10d356
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-27 15:17:59 -0400

    Reeborg's - Escaping the Maze

--- notes.py
+++ notes.py
@@ -63,24 +63,43 @@ while not at_goal():
 # EXERCISE: Reeborg's World Challenge - Hurdle 4
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+
+# Reeborg's World: Maze
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
-def jump():
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
-    move()
-    turn_right()
-    while front_is_clear():
-        move()
-    turn_left()
-    
+
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    if right_is_clear():
+        turn_right()
         move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
