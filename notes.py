f155669e8eac33c406e34d7734d51d2e0a2c3840
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-26 20:23:41 -0400

    reeborg - simplify code

--- notes.py
+++ notes.py
@@ -62,6 +62,7 @@ while not at_goal():
 
 # EXERCISE: Reeborg's World Challenge - Hurdle 4
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
 def turn_right():
     turn_left()
     turn_left()
@@ -69,26 +70,17 @@ def turn_right():
     
 def jump():
     turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
     move()
     turn_right()
-
-while not at_goal():
-    if front_is_clear:
-        (move)
-        while wall_in_front():
-            jump()
-            if front_is_clear():
-                move()
-                if right_is_clear():
-                    turn_right()
-                    while front_is_clear():
-                        move()
-                        if right_is_clear():
-                            turn_right()
-                        while wall_in_front():
-                            turn_left()
-                            if at_goal():
-                                done()
+    while front_is_clear():
         move()
-    else:
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
         jump()
+    else:
+        move()
